29e78b705e5fabfc6ae03071b62da723ea6df3b5
PatGally <149409313+PatGally@users.noreply.github.com>
2023-11-03 19:27:19 -0700

    Final update for strategy pattern

--- Strategy/src/Main.java
+++ Strategy/src/Main.java
@@ -7,26 +7,43 @@ class Course{
     public Course(String num, int cred){
         _cnum = num;  _credits = cred;
     }
-    public void setNumber(String num){_cnum = num;}
-    public void setCredits(int cred){_credits = cred;}
-    public String getNumber(){return _cnum;}
-    public int getCredits(){return _credits;}
-    public String toString(){return _cnum + " " + _credits;}
+    public void setNumber(String num){
+    	_cnum = num;
+    }
+    public void setCredits(int cred){
+    	_credits = cred;
+    }
+    public String getNumber(){
+    	return _cnum;
+    }
+    public int getCredits(){
+    	return _credits;
+    }
+    public String toString(){
+    	return _cnum + " " + _credits;
+    }
 }
 class Student{
     private String _sid;
     public Student(){}
-    public Student(String d){_sid = d;}
-    public void setID(String id){_sid = id;}
-    public String getID(){return _sid;}
-    public String toString(){return _sid;}
+    public Student(String d){
+    	_sid = d;
+    }
+    public void setID(String id){
+    	_sid = id;
+    }
+    public String getID(){
+    	return _sid;
+    }
+    public String toString(){
+    	return _sid;
+    }
 }
 interface SearchBehavior<T, S>
 {
     //T is the object S is the value
     boolean search(T obj, S v);
 }
-
 class StudentSearch implements SearchBehavior<Student, String>
 {
     @Override 
@@ -43,11 +60,16 @@ class CourseSearch implements SearchBehavior<Course, String>
         return obj.getNumber().equals(v);
     }
 }
-
-
+class CnumSearch implements SearchBehavior<String, String>{
+	
+	@Override
+	public boolean search(String obj, String v) {
+		return obj.equals(v);
+	}
+}
 class AllItems<T> //T is data type variable // Generic
 {
-    private ArrayList<T> _items; //Generic because can hold many diff things
+    private ArrayList<T> _items; //Generic because can hold many different things
 
     public AllItems(){_items = new ArrayList<T>();}
     
@@ -59,17 +81,8 @@ class AllItems<T> //T is data type variable // Generic
     {
         _items.add(t);
     }
-    public <S> boolean isItem(S v, SearchBehavior<T, S> sb) // search
-    {
-        for (T item : _items)
-        {
-            if (sb.search(item, v))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
+
+    
     public <S> int findItem(S v, SearchBehavior<T, S> sb) // search  -  sb is a polymorphic reference - template and bound at runtime
     {
         for (int i = 0; i< _items.size(); i++)
@@ -87,13 +100,14 @@ class AllItems<T> //T is data type variable // Generic
             _items.remove(i);
     }
 
-    public int size(){return _items.size();}
+    public int size(){
+    	return _items.size();
+    }
     public T getItem(int i)
     {
         return _items.get(i);
     }
 }
-
 class AllStudents{
     private AllItems<Student> _students;
     public AllStudents()
@@ -107,7 +121,11 @@ class AllStudents{
         _students.addItem(new Student(id));
     }
     public boolean isStudent(String id){
-        return  _students.isItem(id, new StudentSearch()); //student search has a class with a method in it
+          int student = _students.findItem(id, new StudentSearch()); //student search has a class with a method in it
+          if(student == -1) 
+        	  return false;
+          else
+        	  return true;
     }
     public int findStudent(String id){
         return _students.findItem(id, new StudentSearch());
@@ -117,7 +135,7 @@ class AllStudents{
         _students.removeItem(i); //not written in generic
 
     }
-    public boolean modifyStudent(String oldId, String newId) {
+    public boolean modifyStudentID(String oldId, String newId) {
     	int i = findStudent(oldId);
     	if(i < 0 )
     		return false;
@@ -145,7 +163,10 @@ class AllCourses{
         _courses.addItem(new Course(cnum, c));
     }
     public boolean isCourse(String cnum){
-        return _courses.isItem(cnum, new CourseSearch());
+        if(_courses.findItem(cnum, new CourseSearch()) == -1)
+        	return false;
+        else
+        	return true;
     }
     public int findCourse(String cnum){
         return _courses.findItem(cnum, new CourseSearch());
@@ -173,8 +194,98 @@ class AllCourses{
         return s;
     }
 }
+class Enrollment{
+    private HashMap<String, AllItems<String>> _enroll;
+    public Enrollment(){_enroll = new HashMap<String, AllItems<String>>();}
+    public void addCourseToStudent(String id, String c){
+        AllItems<String> t = _enroll.get(id);
+        if (t == null)  // student not in enroll
+            t = new AllItems<String>();
+        t.addItem(c);
+        _enroll.put(id, t);
+    }
+    public void dropStudentFromAllCourses(String id){
+        if (_enroll.containsKey(id))
+            _enroll.remove(id);
+    }
+    public boolean dropStudentFromCourse(String id, String cnum){
+        // Drops a student from a course
+        // If student has no more courses then remove student from hashmap
+        AllItems<String> t = _enroll.get(id);
+        int i = t.findItem(cnum, new CnumSearch());
+        if (i == -1)
+            return false;
+        t.removeItem(i);
+        if (t.size() == 0)
+            _enroll.remove(id);
+        return true;
+    }
+    public boolean dropCourseFromAllStudents(String cnum){
+        // Drops course from all students that are enrolled in the course
+        // If student has no more courses then remove student from hashmap
+        // Students that need to be removed will be stored in a temporary arraylist
+        // in order to remove them from hashmap after iterating
+        boolean found = false;
+        // list of student ids to remove from hashmap
+        ArrayList<String> kt = new ArrayList<String>();
+        // extract keys from hashmap to iterator through
+        Set keys = _enroll.keySet();
+        Iterator itr = keys.iterator();
+        while (itr.hasNext()) {
+            found = false;
+            String k = (String)itr.next();
+            AllItems<String> t = _enroll.get(k);
+            int i = t.findItem(cnum, new CnumSearch());
+            if (i != -1) {
+                t.removeItem(i);
+                if (t.size() == 0)
+                    kt.add(k);
+                found = true;
+            }
+        }
+        for (int i=0; i<kt.size(); i++)
+            _enroll.remove(kt.get(i));
+
+        return found;
+    }
+    public String toString(){
+        String s = "Enrollment:\n";
+        Set keys = _enroll.keySet();
+        Iterator itr = keys.iterator();
+        while (itr.hasNext()) {
+            String k = (String)itr.next();
+            AllItems<String> t = _enroll.get(k);
+            s += (k + " ");
+            for (int j=0; j<t.size(); j++)
+                s += (t.getItem(j) + " ");
+            s += "\n";
+        }
+
+        return s;
+    }
+
+}
+
 public class Main {
     public static void main(String[] args) {
+    	Enrollment en = new Enrollment();
+        en.addCourseToStudent("100", "CSC1700");
+        en.addCourseToStudent("100", "CSC2150");
+        en.addCourseToStudent("100", "CSC2300");
+        en.addCourseToStudent("200", "CSC1700");
+        en.addCourseToStudent("200", "CSC2150");
+        en.addCourseToStudent("200", "CSC2300");
+        en.addCourseToStudent("200", "CSC3400");
+        en.addCourseToStudent("300", "CSC1700");
+        en.addCourseToStudent("300", "CSC2150");
+        System.out.println(en);
+        en.dropStudentFromCourse("100", "CSC2150");
+        System.out.println(en);
+        en.dropCourseFromAllStudents("CSC1700");
+        System.out.println(en);
+        en.dropStudentFromAllCourses("300");
+        System.out.println(en);
+
         AllStudents as = new AllStudents();
         AllCourses ac = new AllCourses();
         as.addStudent("100");
@@ -200,6 +311,8 @@ public class Main {
         System.out.println(ac);
         System.out.println("Is Course CSC3250: " + ac.isCourse("CSC3250") );
         System.out.println("Find Course CSC3250: " + ac.findCourse("CSC3250"));
-        
+        as.modifyStudentID("200", "205");
+        System.out.println(as);
+
     }
 }
